60da65a0d7467d4a77bbe3babe2c4812133058da
Swe <swe@Swes-Mac-mini.local>
2024-03-03 00:22:19 +0800

    deploying html in server

--- server/src/app.js
+++ server/src/app.js
@@ -2,6 +2,7 @@
 const planetsRouter = require("./routes/planets/planets.router");
 const express = require("express");
 const cors = require("cors");
+const path = require("path");
 
 const app = express();
 
@@ -13,7 +14,17 @@ app.use(
   })
 );
 app.use(express.json());
+
+//assigning the html template
+app.use(express.static(path.join(__dirname, "..", "public")));
+
 app.use(planetsRouter);
+
+//due to issue with the site not directly showing the content of the page due to the routing
+//here is how to resolved it
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "..", "public", "index.html"));
+});
 
 module.exports = {
   app,
--- server/src/server.js
+++ server/src/server.js
@@ -5,7 +5,7 @@ const http = require("http");
 
 //Make port configurable, it will run on the available port
 //But if unable to find available port then it will run in the default which is 8000
-const PORT = process.env.PORT || 8100;
+const PORT = process.env.PORT || 8000;
 
 //create Server
 const server = http.createServer(app);
