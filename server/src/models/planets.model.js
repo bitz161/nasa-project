ac3369f4b12f3398ab3c2c95ac735a7d57b1a52a
Swe <swe@Swes-Mac-mini.local>
2024-03-02 23:01:08 +0800

    Planets Data

--- server/src/models/planets.model.js
+++ server/src/models/planets.model.js
@@ -1,3 +1,56 @@
-const planets = [];
+//IMPORTS
+const { parse } = require("csv-parse");
+const fs = require("fs");
+const path = require("path");
 
-module.exports = { planets };
+const habitablePlanets = [];
+
+//boolean, filter keplars based on study if habitable.
+function isHabitablePlanet(planet) {
+  return (
+    planet["koi_disposition"] === "CONFIRMED" &&
+    planet["koi_insol"] > 0.36 &&
+    planet["koi_insol"] < 1.11 &&
+    planet["koi_prad"] < 1.6
+  );
+}
+
+//created a async function that will wait for the info before proceeding
+function loadPlanetsData() {
+  return new Promise((resolve, reject) => {
+    fs.createReadStream(
+      path.join(__dirname, "..", "..", "data", "kepler_data.csv")
+    )
+      .pipe(
+        //parse is the 3rd party npm
+        parse({
+          comment: "#",
+          columns: true,
+        })
+      )
+      .on("data", (data) => {
+        //pushing data in habitablePlanets array
+        if (isHabitablePlanet(data)) {
+          habitablePlanets.push(data);
+        }
+      })
+      .on("error", (err) => {
+        //error handling
+        console.log(err);
+
+        reject(err);
+      })
+      .on("end", () => {
+        //inform that code is done running
+        console.log(`${habitablePlanets.length} habitable planets found!`);
+
+        //call resolved once the value has been populated and ready to go
+        resolve();
+      });
+  });
+}
+
+module.exports = {
+  loadPlanetsData,
+  planets: habitablePlanets,
+};
--- server/src/server.js
+++ server/src/server.js
@@ -1,15 +1,22 @@
+//Imports
 const { app } = require("./app");
+const { loadPlanetsData } = require("./models/planets.model");
+const http = require("http");
 
 //Make port configurable, it will run on the available port
 //But if unable to find available port then it will run in the default which is 8000
 const PORT = process.env.PORT || 8100;
 
-//build in to node
-const http = require("http");
-
 //create Server
 const server = http.createServer(app);
 
-server.listen(PORT, () => {
-  console.log(`Listening on ${PORT}...`);
-});
+//load the datas before proceeding due to data that might take time
+async function startServer() {
+  await loadPlanetsData();
+
+  server.listen(PORT, () => {
+    console.log(`Listening on ${PORT}...`);
+  });
+}
+
+startServer();
